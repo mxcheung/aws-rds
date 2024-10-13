d668c6ceabc8def3ebb68d0f5f1723afce9d86f2
mxcheung <mxcheung@users.noreply.github.com>
2024-10-13 14:51:29 +1100

    Update set_up.sh

--- postgres-esg/ecs-app/set_up.sh
+++ postgres-esg/ecs-app/set_up.sh
@@ -5,3 +5,10 @@
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
 
 sudo yum -y install git
+
+export aws_access_key_id=AKIAVTSSZZZZZZZZZZZZZZ
+export aws_secret_access_key=HqQCiMHHxCZZZZZZZZZZZZZZZZZ
+git clone https://github.com/mxcheung/aws-rds.git
+cd /home/ec2-user/aws-rds/postgres-esg/user_credentials/
+. ./set_up.sh
+
