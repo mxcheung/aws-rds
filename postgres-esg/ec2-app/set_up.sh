686d96251d4f9e7c4fb8ff0462bcbab91e08716d
mxcheung <mxcheung@users.noreply.github.com>
2024-10-13 15:06:20 +1100

    Update set_up.sh

--- postgres-esg/ec2-app/set_up.sh
+++ postgres-esg/ec2-app/set_up.sh
@@ -7,7 +7,12 @@ VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your 
 sudo yum update -y
 sudo yum -y install python3-pip python3-devel nginx git
 
+# Install Gunicorn to serve the Django application.
+pip3 install gunicorn
+
+# Install Django using pip3 and create your Django project.
 pip3 install django
+
 
 cd /home/ec2-user
 django-admin startproject myproject
